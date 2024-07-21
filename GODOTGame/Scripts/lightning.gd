deb1507f5757fbb1dc77ec5e0a6caf42b5d48f91
Trey Brummett <treybrummett@gmail.com>
2024-07-20 22:43:36 -0500

    Fix warning

--- GODOTGame/Scripts/lightning.gd
+++ GODOTGame/Scripts/lightning.gd
@@ -8,7 +8,7 @@ extends Area2D
 func _ready():
 	pass
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
 	$Line2D.points[0] = mage.global_position
 	$Line2D.points[1] = barb.global_position
 	$CollisionShape2D.shape.a = $Line2D.points[0]
